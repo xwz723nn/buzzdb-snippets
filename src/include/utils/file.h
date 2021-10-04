13e15e93e7b2bcf8be4d034609e9256fd315c489
arulraj <arulraj@gatech.edu>
2021-10-04 16:42:46 +0530

    Init

---
+++ src/include/utils/file.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+namespace buzzdb{
+namespace utils{
+
+void hello_world();
+
+}  // namespace utils
+}  // namespace buzzdb
---
+++ src/utils/file.cc
@@ -0,0 +1,16 @@
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
+namespace buzzdb {
+namespace utils {
+
+#define UNUSED(p) ((void)(p))
+
+void hello_world(){
+  printf("Hello world");
+}
+
+}  // namespace utils
+}  // namespace buzzdb
