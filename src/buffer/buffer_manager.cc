7b3818880357bc3292cbb69a7ea21e3460acedaa
arulraj <arulraj@gatech.edu>
2022-01-16 22:07:02 -0500

    checkpoint

---
+++ src/buffer/buffer_manager.cc
@@ -0,0 +1,20 @@
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
+#include <string>
+
+namespace buzzdb {
+namespace utils {
+
+#define UNUSED(p) ((void)(p))
+
+
+
+void hello_world(){
+  printf("Hello world");
+}
+
+}  // namespace utils
+}  // namespace buzzdb
